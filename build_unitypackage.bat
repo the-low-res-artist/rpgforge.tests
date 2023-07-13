41648c765f0d8a2f50383446fce151bdbf360a5f
the-low-res-artist <pixelart.asset@gmail.com>
2023-07-13 12:16:51 +0200

    build + push automatic

--- build_unitypackage.bat
+++ build_unitypackage.bat
@@ -8,11 +8,16 @@ set TEST_PKG_NAME=RPGPowerForgeUnityTestRunner.unitypackage
 :: =================================================================================================
 :: Clear previous package if exists
 if exist %TEST_PKG_NAME%.unitypackage del %TEST_PKG_NAME%
+if exist "%TEST_PROJECT_ROOT_PATH%\%TEST_PKG_NAME%" del "%TEST_PROJECT_ROOT_PATH%\%TEST_PKG_NAME%"
 
 :: =================================================================================================
 :: Build package
 set OPTIONS=-projectPath "%TEST_PROJECT_ROOT_PATH%" -exportPackage Assets\Tests %TEST_PKG_NAME% -quit
 "%UNITY_EXE%" %ARGS%
+
+:: =================================================================================================
+:: Move the package in the root dir
+move "%TEST_PROJECT_ROOT_PATH%\%TEST_PKG_NAME%" %TEST_PKG_NAME%
 
 :: =================================================================================================
 :: export to github
